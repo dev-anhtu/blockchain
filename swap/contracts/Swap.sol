075ae52dd039e432eeab07792187ca2cc0596bbd
key <tunahe140525@fpt.edu.vn>
2023-12-29 13:22:51 +0700

    feat: swap

--- swap/contracts/Swap.sol
+++ swap/contracts/Swap.sol
@@ -1,46 +1,56 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.20;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-import 'hardhat/console.sol';
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
 contract Swap {
+    event Funded(address indexed sender, uint bnbAmount, uint usdtAmount);
+
     IERC20 public bnbToken;
     IERC20 public usdtToken;
 
-    uint256 public bnbAmount;
-    uint256 public usdtAmount;
+    uint public bnbAmount;
+    uint public usdtAmount;
+    uint public k;
 
-    uint256 public k;
-
-    function initialize(address _bnbAddress, address _usdtAddress, uint256 _bnbAmount, uint256 _usdtAmount) public {
-        bnbToken = IERC20(_bnbAddress);
-        usdtToken = IERC20(_usdtAddress);
-
-        bnbToken.transferFrom(msg.sender, address(this), _bnbAmount);
-        usdtToken.transferFrom(msg.sender, address(this), _usdtAmount);
-
-        bnbAmount = _bnbAmount;
-        usdtAmount = _usdtAmount;
-
-        k = bnbAmount * usdtAmount;
+    constructor(IERC20 _bnbToken, IERC20 _usdtToken) {
+        bnbToken = _bnbToken;
+        usdtToken = _usdtToken;
     }
 
-    function buy(uint256 _bnbAmount) public {
-        require(_bnbAmount > 0);
-        require(_bnbAmount <= bnbAmount, 'not enough bnb');
+    modifier hasBeenFunded() {
+        require(bnbAmount > 0 && usdtAmount > 0, 'Contract has not been funded yet');
+        _;
+    }
+
+    function fund(uint _bnbAmount, uint _usdtAmount) public {
+        require(_bnbAmount > 0, 'Invalid BNB amount');
+        require(_usdtAmount > 0, 'Invalid USDT amount');
+
+        require(bnbToken.transferFrom(msg.sender, address(this), _bnbAmount), 'BNB transfer failed');
+        require(usdtToken.transferFrom(msg.sender, address(this), _usdtAmount), 'USDT transfer failed');
+
+        bnbAmount += _bnbAmount;
+        usdtAmount += _usdtAmount;
+
+        k = bnbAmount * usdtAmount;
+
+        emit Funded(msg.sender, _bnbAmount, _usdtAmount);
+    }
+
+    function buy(uint256 _bnbAmount) public hasBeenFunded {
+        require(_bnbAmount > 0, 'Invalid BNB amount');
+        require(_bnbAmount < bnbAmount, 'Not enough BNB');
 
         uint256 newBnbAmount = bnbAmount - _bnbAmount;
-        uint256 _usdtAmount = (k / newBnbAmount) - usdtAmount;
+        uint256 _usdtAmount = k / newBnbAmount - usdtAmount;
 
-        console.log(usdtToken.balanceOf(msg.sender));
-        console.log(_usdtAmount);
-        usdtToken.transfer(address(this), _usdtAmount);
-        console.log(usdtToken.balanceOf(msg.sender));
+        require(usdtToken.balanceOf(msg.sender) >= _usdtAmount, 'Your USDT balance is not enough');
+        require(usdtToken.transfer(address(this), _usdtAmount), 'USDT transfer failed');
 
         bnbToken.transfer(msg.sender, _bnbAmount);
 
-        usdtAmount = usdtAmount + _usdtAmount;
+        usdtAmount += _usdtAmount;
         bnbAmount = newBnbAmount;
     }
 }
--- swap/test/Swap.ts
+++ swap/test/Swap.ts
@@ -2,49 +2,76 @@ import { loadFixture } from '@nomicfoundation/hardhat-network-helpers'
 import { expect } from 'chai'
 import { parseUnits } from 'ethers'
 import { ethers } from 'hardhat'
+import { Swap, Token } from '../typechain-types'
+import { HardhatEthersSigner } from '@nomicfoundation/hardhat-ethers/signers'
+import { before } from 'mocha'
+
+const BNB_DECIMALS: bigint = BigInt(5)
+const USDT_DECIMALS: bigint = BigInt(8)
 
 describe('Swap', function () {
-  async function deployTokenAndContract() {
-    const [owner, account1, account2] = await ethers.getSigners()
+  let contract: Swap
+  let usdt: Token
+  let bnb: Token
+
+  let owner: HardhatEthersSigner
+  let account1: HardhatEthersSigner
+  let account2: HardhatEthersSigner
+
+  before(async function () {
+    ;[owner, account1, account2] = await ethers.getSigners()
 
     const bnbFactory = await ethers.getContractFactory('Token')
-    const bnb = await bnbFactory.deploy('BNB', 'BNB', 8)
+    bnb = await bnbFactory.deploy('BNB', 'BNB', BNB_DECIMALS)
 
     const usdtFactory = await ethers.getContractFactory('Token')
-    const usdt = await usdtFactory.deploy('USDT', 'USDT', 12)
+    usdt = await usdtFactory.deploy('USDT', 'USDT', USDT_DECIMALS)
 
     const swapFactory = await ethers.getContractFactory('Swap')
-    const contract = await swapFactory.deploy()
+    contract = await swapFactory.deploy(await bnb.getAddress(), await usdt.getAddress())
 
-    bnb.transfer(account1, parseUnits('200', 8))
-    usdt.transfer(account1, parseUnits('30000', 12))
+    await bnb.connect(owner).approve(await contract.getAddress(), parseUnits('200', BNB_DECIMALS))
+    await usdt.connect(owner).approve(await contract.getAddress(), parseUnits('30000', USDT_DECIMALS))
 
-    bnb.transfer(account2, parseUnits('200', 8))
-    usdt.transfer(account2, parseUnits('10000', 12))
+    await contract.connect(owner).fund(parseUnits('200', BNB_DECIMALS), parseUnits('30000', USDT_DECIMALS))
 
-    bnb.approve(await contract.getAddress(), parseUnits('200', 8))
-    usdt.approve(await contract.getAddress(), parseUnits('30000', 12))
-
-    await contract.initialize(await bnb.getAddress(), await usdt.getAddress(), parseUnits('200', 8), parseUnits('30000', 12))
     return { contract, bnb, usdt, owner, account1, account2 }
-  }
+  })
 
   it('init contract', async function () {
-    const { contract } = await loadFixture(deployTokenAndContract)
+    expect(await contract.bnbAmount()).to.equal(parseUnits('200', BNB_DECIMALS))
+    expect(await contract.usdtAmount()).to.equal(parseUnits('30000', USDT_DECIMALS))
+    expect(await contract.k()).to.equal(parseUnits('200', BNB_DECIMALS) * parseUnits('30000', USDT_DECIMALS))
+  })
 
-    expect(await contract.bnbAmount()).to.equal(parseUnits('200', 8))
-    expect(await contract.usdtAmount()).to.equal(parseUnits('30000', 12))
-    expect(await contract.k()).to.equal(parseUnits('200', 8) * parseUnits('30000', 12))
+  it('should transfer token', async function () {
+    await bnb.connect(owner).transfer(account1.address, parseUnits('200', BNB_DECIMALS))
+    await usdt.connect(owner).transfer(account1.address, parseUnits('30000', USDT_DECIMALS))
+
+    expect(await bnb.balanceOf(account1.address)).to.equal(parseUnits('200', BNB_DECIMALS))
+    expect(await usdt.balanceOf(account1.address)).to.equal(parseUnits('30000', USDT_DECIMALS))
   })
 
   it('should not enough bnb', async function () {
-    const { contract, account1 } = await loadFixture(deployTokenAndContract)
-    await expect(contract.connect(account1).buy(parseUnits('201', 8))).to.be.revertedWith('not enough bnb')
+    await expect(contract.connect(account1).buy(parseUnits('201', BNB_DECIMALS))).to.be.revertedWith('Not enough BNB')
   })
 
-  it('should enough bnb', async function () {
-    const { bnb, owner, usdt, contract, account1 } = await loadFixture(deployTokenAndContract)
-    await contract.connect(account1).buy(parseUnits('1', 8))
-    expect(await bnb.balanceOf(account1.address)).to.equal(parseUnits('202', 8))
+  it('should usdt transfer not enough', async function () {
+    await expect(contract.connect(account1).buy(parseUnits('199', BNB_DECIMALS))).to.be.revertedWith(
+      'Your USDT balance is not enough'
+    )
   })
+
+  it('should succesfully', async function () {
+    await contract.connect(account1).buy(parseUnits('1', BNB_DECIMALS))
+    expect(await contract.bnbAmount()).to.equal(parseUnits('199', BNB_DECIMALS))
+    expect(await bnb.balanceOf(account1.address)).to.equal(parseUnits('201', BNB_DECIMALS))
+  })
+
+  // it('should enough bnb', async function () {
+  //   const { bnb, owner, usdt, contract, account1 } = await loadFixture(deployTokenAndContract)
+  //   await contract.connect(account1).buy(parseUnits('1', 8))
+  //   usdt.connect(account1).approve(await contract.getAddress(), parseUnits('10000', 12))
+  //   expect(await bnb.balanceOf(account1.address)).to.equal(parseUnits('201', 8))
+  // })
 })
